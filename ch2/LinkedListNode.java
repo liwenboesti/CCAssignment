22ce15c5c0a79474f567cb759f3f66a8c5134fe5
Esti <liwenboesti@users.noreply.github.com>
2015-09-20 23:50:58 -0400

    Create LinkedListNode.java

---
+++ ch2/LinkedListNode.java
@@ -0,0 +1,25 @@
+package Chapter2;
+
+/*
+Name: Wenbo (Esti) Li
+Date: 2015/09/12
+*/
+
+public class LinkedListNode 
+{
+	/* Self created LinkedListNode class
+	 * 
+	 */
+	
+	protected int data;
+	protected LinkedListNode next;
+	
+	protected LinkedListNode()
+	{		
+	}
+	
+	protected LinkedListNode(int val) 
+	{ 
+		data = val;
+	}
+}
